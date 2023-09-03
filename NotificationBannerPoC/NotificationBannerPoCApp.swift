39404a2efb549654595026dc81044b56e26cf040
dacharyc <dc@dacharycarey.com>
2023-09-03 11:09:49 -0400

    Initial commit

---
+++ NotificationBannerPoC/NotificationBannerPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationBannerPoCApp.swift
+//  NotificationBannerPoC
+//
+//  Created by Dachary Carey on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationBannerPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
